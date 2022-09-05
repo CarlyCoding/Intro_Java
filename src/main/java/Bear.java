22c270762b884d71fe7a51c3dc919555a275cc7c
CarlyCoding <carly_off@outlook.com>
2022-09-05 14:19:28 +0100

    inital commit

---
+++ src/main/java/Bear.java
@@ -0,0 +1,17 @@
+public class Bear {
+//    The members of the class go here
+   private String name;
+//    This is the constructor (method that gets run when you create a new bear)
+
+    public Bear(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args){
+        Bear myBear = new Bear("Balu");
+        System.out.println(myBear.getName());
+    }
+}
